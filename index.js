463235febce072173716625b72147e433a77a78e
A K M Shafee Ullah <187232344+shafee-ullah@users.noreply.github.com>
2025-07-27 10:30:38 +0600

    3000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 // Load environment variables from .env file
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
